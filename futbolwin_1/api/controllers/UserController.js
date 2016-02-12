136af317dedccc3ed66ed4308e0bb80c39a85406
luis sanchez <luiswebcam@hotmail.com>
2016-02-12 16:40:43 -0500

    ejemplo con ORM
    utilizando el orm con base de datos mysql

--- futbolwin_1/api/controllers/UserController.js
+++ futbolwin_1/api/controllers/UserController.js
@@ -6,12 +6,12 @@
  */
 
 module.exports = {
-			'new': function (req, res) {
+			 'new': function (req, res) {
               	
               	res.view();
             
             },
-            index: function (req, res) {
+        index: function (req, res) {
             
               User.find(function foundUser (err, users) {
                 if(err) return res.redirect('/user/new');
@@ -21,13 +21,61 @@ module.exports = {
               });
             
             },
-            create: function(req, res) {
+        create: function(req, res) {
             
-                User.create(req.params.all(), function userCreated(err, user) {
-                  if (err) return res.redirect('/user/new');
-                  res.redirect('/user/show/' + user.id); 
-                });
+                User.create(
+                                req.params.all()
+                                , function userCreated(err, user) {
+                                     
+                                            if (err) return res.redirect('/user/new');
+                                           
+                                            res.redirect('/user/show/' + user.id); 
+                                      
+                                      }
+                          );
             
+              },
+        show: function(req, res){
+
+                  funtion_show( req, res);
+                    // res.view({
+                    
+                    //     sku: val.id
+                    
+                    // });
+                   // return res.redirect('/user/new'req.params.id);
               }
 };
+
+
+function funtion_show (req, res) 
+{
+                  var val   = req.params.all();
+
+              User.findOne(
+                                {
+                                    id:val.id
+                                }).exec(function( err, userFinded){
+                                      
+                                      
+                                                      if(err){
+                                                          console.log("Error vida HP!!! '"+err+"'");
+                                                          return res.serverError(err);
+                                                         
+                                                      }
+
+                                                      
+
+                                                      console.log("Encontró al peluche el usuario '"+userFinded+"'")
+                                                      
+
+
+                                                      res.view({
+                                                      
+                                                        a_data: userFinded
+                                                      
+                                                      })
+                                      })
+}
+
 
--- futbolwin_1/api/models/User.js
+++ futbolwin_1/api/models/User.js
@@ -5,41 +5,69 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
+// module.exports = {
+//   connection: 'mysql',
+//  attributes: {
+
+//            id: {
+//             type: 'integer',
+//             autoIncrement: true,
+//             primaryKey: true
+//             },
+//             username: {
+//                 type: 'string'
+//             },
+//             email: {
+//                 type: 'string'
+//             },
+//             password: {
+//                 type: 'string'
+//             },
+
+//             // Este método es para evitar pasar toda la información del modelo
+//             // Evitamos pasar los siguientes parámetros: password, confirmation, encryptedpassword y _csrf. 
+//             toJSON: function() { 
+//               var obj = this.toObject();
+//               delete obj.password;
+//               delete obj.confirmation;
+//               delete obj.encryptedPassword;
+//               delete obj._csrf;
+//               return obj;
+//             }
+//           }
+//      };
+
 module.exports = {
+ 
+    connection: 'mysql',
+    attributes: {
+        id: {
+            type: 'integer',
+            autoIncrement: true,
+            primaryKey: true
+        },
+        username: {
+            type: 'string'
+        },
+        lastname: {
+            type: 'string'
+        },
+        email: {
+            type: 'string'
+        },
+        password: {
+            type: 'string'
+        },
+        toJSON: function() {
 
- attributes: {
-
-            name: {
-              type: 'string',
-              required: true
-            },
-            lastname: {
-              type: 'string',
-              required: true
-            },
-            email: {
-              type: 'string',
-              required: true,
-              unique: true
-            },
-            encryptedPassword: {
-              type: 'string'
-            },
-
-            // Este método es para evitar pasar toda la información del modelo
-            // Evitamos pasar los siguientes parámetros: password, confirmation, encryptedpassword y _csrf. 
-            toJSON: function() { 
-              var obj = this.toObject();
-              delete obj.password;
-              delete obj.confirmation;
-              delete obj.encryptedPassword;
-              delete obj._csrf;
-              return obj;
+                var obj = this.toObject();
+                delete obj.password;
+                return obj;
+            
             }
-          }
-     };
 
-
+    }
+};
 
      
 
